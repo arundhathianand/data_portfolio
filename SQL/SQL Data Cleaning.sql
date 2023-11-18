e6ca2cc2d117455a628e4315c5172c4828f32272
arundhathianand <74897974+arundhathianand@users.noreply.github.com>
2023-11-18 19:03:21 +0100

    Update SQL Data Cleaning.sql

--- SQL/SQL Data Cleaning.sql
+++ SQL/SQL Data Cleaning.sql
@@ -1,28 +1,28 @@
 
-SELECT * FROM sreeTest.NashvilleHousing
+SELECT * FROM NashvilleHousing
 
 -- Standardise Date Column
 
-SELECT SaleDate, STR_TO_DATE(SaleDate, '%M %e, %Y') FROM sreeTest.NashvilleHousing
+SELECT SaleDate, STR_TO_DATE(SaleDate, '%M %e, %Y') FROM NashvilleHousing
 
-UPDATE sreeTest.NashvilleHousing
+UPDATE NashvilleHousing
 SET SaleDate = STR_TO_DATE(SaleDate, '%M %e, %Y')
 
 -- Clean Property Address Column 
 
-SELECT * FROM sreeTest.NashvilleHousing
+SELECT * FROM NashvilleHousing
 order by ParcelID 
 
 SELECT NH_A.ParcelID, NH_A.PropertyAddress, NH_B.ParcelID, NH_B.PropertyAddress, IF(NH_A.PropertyAddress = '', 'Y', 'N')
-FROM sreeTest.NashvilleHousing NH_A
-JOIN sreeTest.NashvilleHousing NH_B
+FROM NashvilleHousing NH_A
+JOIN NashvilleHousing NH_B
 	ON NH_A.ParcelID = NH_B.ParcelID AND NH_A.UniqueID <> NH_B.UniqueID
 WHERE NH_A.PropertyAddress = ''
 
 -- Self-join and assign missing PropertyAddress based on matching ParcelID
 
-UPDATE sreeTest.NashvilleHousing NH_A 
-JOIN sreeTest.NashvilleHousing NH_B
+UPDATE NashvilleHousing NH_A 
+JOIN NashvilleHousing NH_B
 	ON NH_A.ParcelID = NH_B.ParcelID AND NH_A.UniqueID <> NH_B.UniqueID
 SET NH_A.PropertyAddress = IF(NH_A.PropertyAddress = '', NH_B.PropertyAddress, NH_A.PropertyAddress)
 WHERE NH_A.PropertyAddress = ''
@@ -32,51 +32,51 @@ WHERE NH_A.PropertyAddress = ''
 SELECT 
 SUBSTRING_INDEX(PropertyAddress, ',', 1) AS Address,
 SUBSTRING_INDEX(PropertyAddress, ',', -1) AS City
-FROM sreeTest.NashvilleHousing
+FROM NashvilleHousing
 
-ALTER TABLE sreeTest.NashvilleHousing
+ALTER TABLE NashvilleHousing
 ADD PropertySplitAddress text;
 
-UPDATE sreeTest.NashvilleHousing
+UPDATE NashvilleHousing
 SET PropertySplitAddress = SUBSTRING_INDEX(PropertyAddress, ',', 1)
 
-ALTER TABLE sreeTest.NashvilleHousing
+ALTER TABLE NashvilleHousing
 ADD PropertySplitCity text;
 
-UPDATE sreeTest.NashvilleHousing
+UPDATE NashvilleHousing
 SET PropertySplitCity = SUBSTRING_INDEX(PropertyAddress, ',', -1)
 
 -- Breaking Owner Address into Address, City and STATE
 
-SELECT OwnerAddress FROM sreeTest.NashvilleHousing
+SELECT OwnerAddress FROM NashvilleHousing
 
 SELECT 
 SUBSTRING_INDEX(OwnerAddress, ',', 1) AS OAddress,
 SUBSTRING_INDEX(SUBSTRING_INDEX(OwnerAddress, ',', 2), ',', -1) AS OCity,
 SUBSTRING_INDEX(OwnerAddress, ',', -1) AS OState
-FROM sreeTest.NashvilleHousing
+FROM NashvilleHousing
 
-ALTER TABLE sreeTest.NashvilleHousing
+ALTER TABLE NashvilleHousing
 ADD OwnerSplitAddress text;
 
-UPDATE sreeTest.NashvilleHousing
+UPDATE NashvilleHousing
 SET OwnerSplitAddress = SUBSTRING_INDEX(OwnerAddress, ',', 1)
 
-ALTER TABLE sreeTest.NashvilleHousing
+ALTER TABLE NashvilleHousing
 ADD OwnerSplitCity text;
 
-UPDATE sreeTest.NashvilleHousing
+UPDATE NashvilleHousing
 SET OwnerSplitCity = SUBSTRING_INDEX(SUBSTRING_INDEX(OwnerAddress, ',', 2), ',', -1)
 
-ALTER TABLE sreeTest.NashvilleHousing
+ALTER TABLE NashvilleHousing
 ADD OwnerSplitState text;
 
-UPDATE sreeTest.NashvilleHousing
+UPDATE NashvilleHousing
 SET OwnerSplitState = SUBSTRING_INDEX(OwnerAddress, ',', -1)
 
 -- Standardise SoldAsVacant column entries 
 
- SELECT DISTINCT(SoldAsVacant), COUNT(SoldAsVacant) FROM sreeTest.NashvilleHousing
+ SELECT DISTINCT(SoldAsVacant), COUNT(SoldAsVacant) FROM NashvilleHousing
  GROUP BY SoldAsVacant
  
  SELECT SoldAsVacant,
@@ -84,7 +84,7 @@ SET OwnerSplitState = SUBSTRING_INDEX(OwnerAddress, ',', -1)
  		 WHEN SoldAsVacant = 'N' THEN 'No'
  		 ELSE SoldAsVacant
 		 END
- FROM sreeTest.NashvilleHousing
+ FROM NashvilleHousing
  
  UPDATE NashvilleHousing
  SET SoldAsVacant = CASE WHEN SoldAsVacant = 'Y' THEN 'Yes'
@@ -98,31 +98,31 @@ With ABC AS (
 SELECT *,
 	ROW_NUMBER() OVER (
 	PARTITION BY ParcelID, PropertyAddress, SalePrice, SaleDate, LegalReference ORDER BY UniqueID) row_num
-FROM sreeTest.NashvilleHousing
+FROM NashvilleHousing
 )
 SELECT UniqueID from ABC WHERE row_num >1
 
 -- Remove Duplicates
 
-DELETE FROM sreeTest.NashvilleHousing
+DELETE FROM NashvilleHousing
 WHERE UniqueID in (
 	SELECT UniqueID from (
 		SELECT UniqueID,
 			   ROW_NUMBER() OVER (
 					PARTITION BY ParcelID, PropertyAddress, SalePrice, SaleDate, LegalReference ORDER BY UniqueID) row_num
-		FROM sreeTest.NashvilleHousing
+		FROM NashvilleHousing
 		) dummy
 	WHERE row_num >1
 )
 
 -- Remove useless columns
 
-ALTER TABLE sreeTest.NashvilleHousing
+ALTER TABLE NashvilleHousing
 	DROP OwnerAddress,
 	DROP PropertyAddress,
 	DROP  TaxDistrict
 	
-ALTER TABLE sreeTest.NashvilleHousing
+ALTER TABLE NashvilleHousing
 	DROP SaleDate
 	
 
